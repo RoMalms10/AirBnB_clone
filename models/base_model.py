7f11d23cd4f363dee3d328c1129631fec468aca3
shannonanderson77 <shannonanderson77@gmail.com>
2018-02-09 03:45:49 +0000

    fix output, add docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
+"""
+A module that handles random id generation, datetime created, datetime
+updated, and dictionary instantiation of a BaseModel
+    Args:
+        none
+    Returns:
+        instance dictionary
+"""
 import datetime
 import uuid
 
 
 class BaseModel():
     """A BaseModel class to serve as the base of all models"""
-    id="None"
-    created_at=None
-    updated_at=None
+    id = "None"
+    created_at = None
+    updated_at = None
 
     def __init__(self):
+        """Initialize instance random id generation and datetime created"""
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now().isoformat())
-        self.updated_at = str(datetime.datetime.now().isoformat())
+        self.created_at = datetime.datetime.now()
 
     def __str__(self):
         """A method that returns a string representation"""
-        return "[{}] [{}] {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """A method that returns time instance updated"""
-        return self.updated_at
+        """A method that updates instance datetime"""
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """A method that returns a dictionary containing all
-            keys/values of __dict__ of the instance"""
+           keys/values of __dict__ of the instance"""
         new_dict = {}
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
         for att in self.__dict__:
             new_dict[att] = getattr(self, att)
         new_dict['__class__'] = self.__class__.__name__
