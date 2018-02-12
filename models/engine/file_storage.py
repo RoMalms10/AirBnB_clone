6add6cc4212e053276e6bc217c1c397fdd8d8424
RoMalms10 <156@holbertonschool.com>
2018-02-12 20:19:38 +0000

    Update pep8 complainces

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,46 +7,29 @@ import os
 
 
 class FileStorage():
-    """
-    """
-
+    """ Class that deals with file storage and loading """
 
     __file_storage = "file.json"
     __objects = {}
 
-
     def all(self):
-        """
-        """
-
-
+        """ Returns the dict containing all objects """
         return self.__objects
 
-
     def new(self, obj):
-        """
-        """
-
-
+        """ Adds new object to dict of current objects """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
-
     def save(self):
-        """
-        """
-
-
+        """ saves all objects into a JSON file """
         with open(self.__file_storage, mode="w", encoding="UTF-8") as f:
             for key, value in self.__objects.items():
                 self.__objects[key] = value.to_dict()
             json.dump(self.__objects, f)
 
-
     def reload(self):
-        """
-        """
-
+        """ Reloads the JSON from the specified file """
         if os.path.isfile(self.__file_storage):
             with open(self.__file_storage, mode="r", encoding="UTF-8") as f:
                 temp_dict = json.load(f)
