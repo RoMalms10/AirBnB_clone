8d733ab09b80eca8fce03deea373d73970940f25
RoMalms10 <156@holbertonschool.com>
2018-02-12 02:32:57 +0000

    Add to __init__ file

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python3
+"""A module that loads storage"""
 
+from models.engine.file_storage import FileStorage
+from .base_model import BaseModel
 
+class_dict = {'BaseModel': BaseModel}
+
+storage = FileStorage()
+storage.reload()
