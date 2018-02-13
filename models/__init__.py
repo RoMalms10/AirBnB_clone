de66488937afb3e2b2c290002042e97f9cdcd131
RoMalms10 <156@holbertonschool.com>
2018-02-13 04:54:22 +0000

    Add classes into class_dict

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,14 @@
 
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
+from .user import User
+from .state import State
+from .city import City
+from .place import Place
+from .amenity import Amenity
+from .review import Review
 
-class_dict = {'BaseModel': BaseModel}
+class_dict = {'BaseModel': BaseModel, 'User': User, 'Review': Review, 'State': State, 'City': City, 'Place': Place, 'Amenity': Amenity, }
 
 storage = FileStorage()
 storage.reload()
