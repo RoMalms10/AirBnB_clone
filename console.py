c5c86bebc4f09065b922e6a384cb2834f2b94f84
RoMalms10 <156@holbertonschool.com>
2018-02-13 01:45:30 +0000

    Update console to include more methods

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ My cmd module """
 import cmd
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +21,94 @@ class HBNBCommand(cmd.Cmd):
         """ Do nothing if empty line """
         pass
 
+    def do_create(self, arg):
+        """ Creates a new instance of BaseModel, saves it and prints the id """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in models.class_dict:
+            print("** class doesn't exist **")
+        else:
+            a = models.class_dict[arg]()
+            models.storage.save()
+            models.storage.reload()
+            print(a.id)
+
+    def do_show(self, arg):
+        """ Prints string representation of instance based on name and id """
+        arg = arg.split()
+        if len(arg) == 0:
+            print ("** class name missing **")
+        elif arg[0] not in models.class_dict:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg[0], arg[1])
+            temp_dict = models.storage.all()
+            if key in temp_dict:
+                print(temp_dict[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """ Destroys an instance based off name and id """
+        arg = arg.split()
+        if len(arg) == 0:
+            print ("** class name missing **")
+        elif arg[0] not in models.class_dict:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg[0], arg[1])
+            temp_dict = models.storage.all()
+            if key in temp_dict:
+                del temp_dict[key]
+                models.storage.save()
+                models.storage.reload()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """ Print all instances of a class (or all intances if no class) """
+        arg = arg.split()
+        temp_dict = models.storage.all()
+        obj_list = []
+        if len(arg) == 0:
+            for key, value in temp_dict.items():
+                obj_list.append(value)
+            print(obj_list)
+        elif arg[0] not in models.class_dict:
+            print("** class doesn't exist **")
+        else:
+            for key, value in temp_dict.items():
+                if arg[0] in key:
+                    obj_list.append(value)
+            print(obj_list)
+
+    def do_update(self, arg):
+        """ Update instance """
+        arg = arg.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in models.class_dict:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(arg[0], arg[1])
+            temp_dict = models.storage.all()
+            if key not in temp_dict:
+                print("** no instance found **")
+            elif len(arg) == 2:
+                print("** attribute name missing **")
+            elif len(arg) == 3:
+                print("** value missing **")
+            else:
+                value = temp_dict.get(key)
+                setattr(value, arg[2], arg[3].strip("\""))
+                models.storage.save()
+                models.storage.reload()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
