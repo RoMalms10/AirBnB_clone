ad9753e65d2395e52beb8ba2887acc3492630b9d
shannonanderson77 <shannonanderson77@gmail.com>
2018-02-15 04:40:21 +0000

    add filestorage test file setup

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+Unittest for Airbnb clone command interpreter FileStorage class
+"""
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+import io
+import os
+import sys
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """A TestFileStorage class with test functions for the FileStorage class"""
+
+    def test_instantiation(self):
+        """Test instantiation of a FileStorage class"""
+        a = FileStorage()
+        self.assertIs(type(a), FileStorage)
+
+    def test_all(self):
+        """Test that all() returns all objects"""
+        a = FileStorage()
+
+    def test_new(self):
+        """Test that new() adds new object to current objects"""
+        a = FileStorage()
+
+    def test_save(self):
+        """Test that save() saves all objects to a JSON file"""
+        a = FileStorage()
+
+    def test_reload(self):
+        """Test that reload() reloads the JSON from file"""
+        a = FileStorage()
